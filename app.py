01dd310247a1476d9bc4b92154ea09de5fda0b80
ajordanb <ajordanbojanic@gmail.com>
2023-04-02 19:01:11 -0500

    handling no data error

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ app = Rocketry()
 
 @app.task(weekly.on("Sunday") & time_of_day.between("06:00", "12:00") | retry(3))
 def do_weekly():
-  '''Runs a task weekly on Sunday between 6 and noon. Max retries if failed: 3'''
+    """Runs a task weekly on Sunday between 6 and noon. Max retries if failed: 3"""
     try:
       data = get_data('SELECT * FROM table_name')
+      if not data:
+         raise Exception("No data found")
       logger.info(data)
       # Do something with data
     except Exception as e:
@@ -18,9 +20,12 @@ def do_weekly():
 
 @app.task(monthly.at("15th") & time_of_day.between("06:00", "08:00") | retry(3))
 def do_monthly():
-  '''Runs a task monthly on the 15th between 6 and 8. Max retries if failed: 3'''
-     try:
+    """Runs a task monthly on the 15th between 6
+    and 8. Max retries if failed: 3"""
+    try:
       data = get_data('SELECT * FROM table_name')
+      if not data:
+         raise Exception("No data found")
       logger.info(data)
       # Do something with data
     except Exception as e:
